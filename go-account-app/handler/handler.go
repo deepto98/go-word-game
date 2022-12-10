8fefb7aa045d1849e925248e81025e2471a44ade
deepto <ddepp98@outlook.com>
2022-12-11 04:33:33 +0530

    added handlers for routes, added env variable for ACCOUNT_API_URL

--- go-account-app/handler/handler.go
+++ go-account-app/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,9 +18,50 @@ func NewHandler(config *Config) {
 
 	handler := &Handler{}
 
-	accountGroup := config.Router.Group("/api/account")
+	accountGroup := config.Router.Group(os.Getenv("ACCOUNT_API_URL"))
 
 	accountGroup.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"hi": "Hello"})
 	})
+	accountGroup.GET("/me", handler.Me)
+	accountGroup.POST("/signup", handler.Signup)
+	accountGroup.POST("/signin", handler.Signin)
+	accountGroup.POST("/signout", handler.Signout)
+	accountGroup.POST("/tokens", handler.Tokens)
+	accountGroup.POST("/image", handler.Image)
+	accountGroup.DELETE("/image", handler.DeleteImage)
+	accountGroup.PUT("/details", handler.Details)
+}
+
+func (handler *Handler) Me(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hi": "Its me"})
+
+}
+
+func (handler *Handler) Signin(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hi": "Signed in"})
+}
+
+func (handler *Handler) Signout(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hi": "Signed in"})
+}
+
+func (handler *Handler) Signup(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hi": "Signed up"})
+}
+
+func (handler *Handler) Tokens(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hi": "Signed in"})
+}
+
+func (handler *Handler) Image(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hi": "Signed in"})
+}
+
+func (handler *Handler) DeleteImage(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hi": "Signed in"})
+}
+
+func (handler *Handler) Details(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hi": "Signed in"})
 }
