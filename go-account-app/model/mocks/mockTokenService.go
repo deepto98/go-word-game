3554b53b5ce9e6e3b76ca205a5e14c8995d879d2
deepto <ddepp98@outlook.com>
2022-12-14 14:23:35 +0530

    added mock for TokenService

---
+++ go-account-app/model/mocks/mockTokenService.go
@@ -0,0 +1,28 @@
+package mocks
+
+import (
+	"github.com/deepto98/go-word-game/go-account-app/model"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockTokenService struct {
+	mock.Mock
+}
+
+//Mocks NewTokenPairFromUser
+func (mockTokenService *MockTokenService) NewTokenPairFromUser(ctx *gin.Context, user *model.User, previousTokenId string) (*model.TokenPair, error) {
+	ret := mockTokenService.Called(ctx, user, previousTokenId)
+
+	var r0 *model.TokenPair
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*model.TokenPair)
+	}
+
+	var r1 error
+	if ret.Get(1) != nil {
+		r1 = ret.Get(1).(error)
+	}
+
+	return r0, r1
+}
